72bfd8c0298a6b3724113e00fd47d2b7a7c9a527
Deivison <d.almeidasantana@gmail.com>
2024-05-20 14:32:41 -0300

    correcao

--- Dessafio01.py
+++ Dessafio01.py
@@ -1,3 +1,5 @@
 nome = input ('Digite seu nome')
-print ('Bem vindo(a)',nome,'prazer em ver você')
+#print ('Bem vindo(a)',nome,'prazer em ver você')
+frase ='Bem vindo(a) '+ nome +' prazer em ver você'
+print (frase,'\n baboseira baboseira')
 
