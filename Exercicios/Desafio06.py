9aba00dcc21c38dae1b1ab5e220d64c925eb4bc7
Deivison <d.almeidasantana@gmail.com>
2024-05-21 18:37:07 -0300

    atualizacao aula 7

---
+++ Exercicios/Desafio06.py
@@ -0,0 +1,5 @@
+#Crie um algortimo que leia um número e mostre o seu dobrp, seu triplo e sua raiz quadrada
+
+num = int(input('Digite um número inteiro'))
+
+print('Seu número é: {}, o dobro dele é {}, o triplo dele é {} e sua raiz quadrada é {}'.format(num, num*2, num*3, num**(1/2)))
