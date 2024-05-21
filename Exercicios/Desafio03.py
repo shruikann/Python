5b7c5fb0d4332ee0fcf022fbe8e5b3bf434a4e56
Deivison <d.almeidasantana@gmail.com>
2024-05-21 12:01:36 -0300

    novo commit

---
+++ Exercicios/Desafio03.py
@@ -0,0 +1,5 @@
+num1 = input ('Digite um número')
+num2 = input ('Digite um número')
+result = num1 + num2
+
+print ('A soma de' ,num1, 'e', num2, 'é igual a', result)
---
+++ Exercicios/Dessafio01.py
@@ -0,0 +1,5 @@
+nome = input ('Digite seu nome')
+#print ('Bem vindo(a)',nome,'prazer em ver você')
+frase ='Bem vindo(a) '+ nome +' prazer em ver você'
+print (frase,'\n baboseira baboseira')
+
