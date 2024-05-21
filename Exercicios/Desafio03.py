cc3afcccb17e8687b734e7a855de8eaf2d6e2eb3
Deivison <d.almeidasantana@gmail.com>
2024-05-21 16:22:59 -0300

    atualizacao

--- Exercicios/Desafio03.py
+++ Exercicios/Desafio03.py
@@ -1,5 +1,5 @@
-num1 = input ('Digite um número')
-num2 = input ('Digite um número')
+num1 = int(input('Digite um número'))
+num2 = int(input('Digite um número'))
 result = num1 + num2
 
-print ('A soma de' ,num1, 'e', num2, 'é igual a', result)
+print('A soma de', num1, 'e', num2, 'é igual a', result)
