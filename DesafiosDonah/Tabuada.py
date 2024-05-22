40724c0cd00fbe0509864ed38cb855e8404a6320
Deivison <d.almeidasantana@gmail.com>
2024-05-22 15:10:22 -0300

    TabuadaDonahComIF

--- DesafiosDonah/Tabuada.py
+++ DesafiosDonah/Tabuada.py
@@ -3,7 +3,13 @@
 tabuada = int(input('Informe a tabuada desejada'))
 inicio = int(input('Informe por onde você quer começar: '))
 final = int(input('Informe onde você quer terminar: '))
+if inicio > final:
+    temp = inicio
+    inicio = final
+    final = temp
 
+    print('Inicio {}'.format(inicio))
+    print('Final {}'.format(final))
 for count in range(inicio, final+1):
     conta =tabuada * count
     print('{} x {} = {}'.format(tabuada, count, conta))
