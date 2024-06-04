96f02e01e6fae2b8ed7980045e5c97155b0b8e34
Deivison <d.almeidasantana@gmail.com>
2024-06-04 13:49:47 -0300

    atualização desafios

--- Exercicios/Desafio033.py
+++ Exercicios/Desafio033.py
@@ -1,1 +1,14 @@
+num_list = []
+
+for tamanho in range(0,3):
+    num = int(input('Digite um número inteiro '))
+    #adicionando elementos a lista
+    num_list.append(num)
+
+#organiza a lista em forma crescente, (reverse=True) organiza a lista descendente
+num_list.sort()
+print('O menor número é {}'.format(num_list[0]))
+print('O maior número é {}'.format(num_list[-1]))
+
+
 
